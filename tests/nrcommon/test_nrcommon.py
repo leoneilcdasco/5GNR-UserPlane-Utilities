c340fd5047528a16860c23880fe1278efbf15d99
leoneilcdasco <leoneilcdasco@gmail.com>
2018-09-06 08:53:40 +0800

    added tests for nrcommon test_hexStringToByteListLittleEndian

--- tests/nrcommon/test_nrcommon.py
+++ tests/nrcommon/test_nrcommon.py
@@ -26,5 +26,13 @@ class TestNrcommon(TestCase):
         act = self.iNrCommon.hexStringToByteListBigEndian(testString)
         self.assertEqual(act, exp)
 
-    #def test_hexStringToByteListLittleEndian(self):
-        # self.pass()
+    def test_hexStringToByteListLittleEndian(self):
+        testString = "12345678"
+        exp = list(reversed([0x12, 0x34, 0x56, 0x78]))
+        act = self.iNrCommon.hexStringToByteListLittleEndian(testString)
+        self.assertEqual(act, exp)
+
+        testString = "1234567"
+        exp = list(reversed([0x01, 0x23, 0x45, 0x67]))
+        act = self.iNrCommon.hexStringToByteListLittleEndian(testString)
+        self.assertEqual(act, exp)
